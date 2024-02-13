20683dccc6f720c717585a1b00a6f81b2b57b1a7
seriaati <seria.ati@gmail.com>
2024-02-13 18:30:25 +0900

    feat: Add image and video file extension constant

---
+++ seria/constants.py
@@ -0,0 +1,15 @@
+IMAGE_EXTENSIONS = (
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".gif",
+    ".webp",
+)
+VIDEO_EXTENSIONS = (
+    ".mp4",
+    ".mov",
+    ".avi",
+    ".wmv",
+    ".webm",
+    ".m4v",
+)
