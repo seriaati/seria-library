2d1012d575ce43a4104efdca20a2ccdac027dda6
seria <seria.ati@gmail.com>
2024-02-16 22:49:33 +0800

    refactor: Improve `silent_save` method
    Return creation result

--- seria/tortoise/model.py
+++ seria/tortoise/model.py
@@ -1,4 +1,3 @@
-import contextlib
 from typing import Any, Self
 
 from tortoise.exceptions import DoesNotExist, IntegrityError
@@ -10,6 +9,7 @@ __all__ = ("Model",)
 class Model(TortoiseModel):
     @classmethod
     async def get_or_create(cls, **kwargs: Any) -> tuple[Self, bool]:
+        """Get an instance of the model by the given parameters or create it if it doesn't exist."""
         try:
             return await cls.get(**kwargs), False
         except DoesNotExist:
@@ -17,11 +17,17 @@ class Model(TortoiseModel):
 
     @classmethod
     async def silent_create(cls, **kwargs: Any) -> Self | None:
+        """Create an instance of the model with the given parameters or return None if it already exists."""
         try:
             return await cls.create(**kwargs)
         except IntegrityError:
             return None
 
-    async def silent_save(self, **kwargs: Any) -> None:
-        with contextlib.suppress(IntegrityError):
-            return await super().save(**kwargs)
+    async def silent_save(self, **kwargs: Any) -> bool:
+        """Save the instance or return False if it already exists."""
+        try:
+            await self.save(**kwargs)
+        except IntegrityError:
+            return False
+        else:
+            return True
